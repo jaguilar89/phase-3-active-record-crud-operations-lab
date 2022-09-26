3c811efb0445abfc8a13ab847441120d4c3404db
Jose Aguilar <jaguilarny@gmail.com>
2022-09-26 00:33:19 -0400

    commit tree f899f68d945a495c00087f1b28308d09da8b3c90

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,45 @@
 class Movie < ActiveRecord::Base
+  def self.create_with_title(title)
+    self.create(title: title)
+  end
 
+  def self.first_movie
+    self.first
+  end
+
+  def self.last_movie
+    self.last
+  end
+
+  def self.movie_count
+    self.all.count
+  end
+
+  def self.find_movie_with_id(id)
+    self.all.find(id)
+  end
+
+  def self.find_movie_with_attributes(args)
+    self.all.find_by(args)
+  end
+
+  def self.find_movies_after_2002
+    self.all.where("release_date > ?", 2002)
+  end
+
+  def update_with_attributes(args)
+    self.update(args)
+  end
+
+  def self.update_all_titles(title)
+    self.all.update(title: title)
+  end
+
+  def self.delete_by_id(id)
+    self.all.destroy(id)
+  end
+
+  def self.delete_all_movies
+    self.all.destroy_all
+  end
 end
